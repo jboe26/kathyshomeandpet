fd98e22d184f3e28b135cebb30be3b557d10b294
Josh Boepple <joshrboepple@outlook.com>
2023-08-14 10:43:48 -0400

    update

--- submit.php
+++ submit.php
@@ -8,16 +8,4 @@ if($_POST["message"]) {
 
 }
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-
-    // Display the submitted data
-    echo "Name: " . $name . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Message: " . $message . "<br>";
-}
 ?>
